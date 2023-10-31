40eeefd04718b72327e892e4717555089282b001
Elhai <elhaimn@gmail.com>
2023-10-31 12:12:13 +0200

    Leet code - Remove Duplicates from Sorted Array II

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello World from noa!!')
+var removeDuplicates = function (nums) {
+  if (nums.length < 3) return nums.length;
+  let l = 0;
+  let r = 0;
+  let count = 1;
+  while (r < nums.length) {
+      while (r + 1 < nums.length && nums[r] === nums[r + 1]) {
+          r++;
+          count++;
+      }
+      let min = Math.min(2, count);
+      for (let i = 0; i < min; i++) {
+          nums[l] = nums[r];
+          l++
+      }
+      r++;
+      count = 1;
+      console.log(nums)
+  }
+  return l;
+};
+
+removeDuplicates([1,1,1,2,2,3]);
