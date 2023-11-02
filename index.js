5b03e3ea40fa2ec60432a9037e33c0b2c6501464
Elhai Mansbach <elhaimn@gmail.com>
2023-11-02 10:33:44 +0200

    Leet code: 367. Valid Perfect Square

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-var removeDuplicates = function (nums) {
-  if (nums.length < 3) return nums.length;
-  let l = 0;
-  let r = 0;
-  let count = 1;
-  while (r < nums.length) {
-    while (r + 1 < nums.length && nums[r] === nums[r + 1]) {
-      r++;
-      count++;
-    }
-    let min = Math.min(2, count);
-    for (let i = 0; i < min; i++) {
-      nums[l] = nums[r];
-      l++;
-    }
-    r++;
-    count = 1;
-    console.log(nums);
+var mySqrt = function (x) {
+  let low = 0;
+  let high = x;
+  while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+      const square = mid * mid;
+      if (square <= x) low = mid + 1;
+      else high = mid - 1;
   }
-  return l;
-};
+  return high;
+}
 
-removeDuplicates([1, 1, 1, 2, 2, 3]);
-removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]);
-removeDuplicates([0, 0, 0, 0, 0, 0, 2, 2, 3, 3]);
-removeDuplicates([0, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3]);
+mySqrt(50);
