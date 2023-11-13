45747088b6af24a4d99ff1e08c6825b36b9bfb1a
Elhai Mansbach <elhaimn@gmail.com>
2023-11-13 11:24:11 +0200

    66. Plus One

---
+++ Ex_66.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  let str = digits.join("");
+  let num = BigInt(str) + 1n;
+  return num.toString().split("").map(Number);
+};
+
+console.log(plusOne([1, 2, 3]));
