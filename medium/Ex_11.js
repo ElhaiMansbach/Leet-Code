4c6fa9a630d8bb3b4c20cfc600c48e7e7b0de4d2
Elhai Mansbach <elhaimn@gmail.com>
2023-11-19 10:07:14 +0200

    11. Container With Most Water

---
+++ medium/Ex_11.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let l = 0,
+    r = height.length - 1,
+    max = 0;
+
+  while (l < r) {
+    max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+    if (height[l] < height[r]) l++;
+    else r--;
+  }
+  return max;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])); // 49
+console.log(maxArea([1, 1])); // 1
