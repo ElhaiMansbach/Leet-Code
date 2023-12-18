d389d326035e3f2c7ea7e016faa2184dea9c1e50
Elhai Mansbach <elhaimn@gmail.com>
2023-12-18 12:32:57 +0200

    783. Minimum Distance Between BST Nodes

---
+++ easy/Ex_783.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDiffInBST = function (root) {
+  let prev = null;
+  let minDiff = Infinity;
+
+  function inorderTraversal(node) {
+    if (!node) return;
+    inorderTraversal(node.left);
+    if (prev !== null) minDiff = Math.min(minDiff, node.val - prev);
+    prev = node.val;
+    inorderTraversal(node.right);
+  }
+
+  inorderTraversal(root);
+  return minDiff;
+};
